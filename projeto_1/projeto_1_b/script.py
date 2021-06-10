dd4b0c66d130ac0c1e052eea22c96a2d5458387f
jhonatheberson <jhonatheberson@outlook.com.br>
2021-06-10 09:52:53 -0300

    update

--- projeto_1/projeto_1_b/script.py
+++ projeto_1/projeto_1_b/script.py
@@ -8,6 +8,73 @@
 #
 # IMPORTANT: for each successful call to simxStart, there
 # should be a corresponding call to simxFinish at the end!
+import math
+import numpy as np
+point_init = (0,0, 0)
+point_end = (10,10, 45)
+
+
+
+
+def polinomio(point_init, point_end):
+    sigma = 1
+    deltaX = point_end[0] - point_init[0]
+    deltaY = point_end[1] - point_init[1]
+    alfa_init = math.tan(point_init[2])
+    alfa_end = math.tan(point_end[2])
+
+    if point_init[2] >= ((math.pi/2) - sigma) and point_init[2] <= ((math.pi/2) + sigma) and point_end[2] >= ((math.pi/2) - sigma) and point_end[2] >= ((math.pi/2) + sigma):
+        print('i')
+        b1 = deltaY
+        b2 = 0
+        a0 = point_init[0]
+        a1 = 0
+        a2 = 3*deltaX
+        a3 = -2*deltaX
+        b0 = point_init[1]
+        b3 = deltaY-b1-b2
+
+    elif point_init[2] >= ((math.pi/2) - sigma) and point_init[2] <= ((math.pi/2) + sigma):
+        print('ii')
+        a3 = -(deltaX/2)
+        b3 = 1
+        a0 = point_init[0]
+        a1 = 0
+        a2 = deltaX-a3
+        b0 = point_init[1]
+        b1 = 2*(deltaY-alfa_end*deltaX) - alfa_end*a3 + b3
+        b3 = (2*alfa_end*deltaX-deltaY) + alfa_end*a3 - 2*b3
+    elif point_end[2] >= ((math.pi/2) - sigma) and point_init[2] <= ((math.pi/2) + sigma):
+        print('iii')
+        a1 = 3*(deltaX/2)
+        b2 = 1
+        a0 = point_init[0]
+        a2 = 3*deltaX - 2*a1
+        a3 = a1 - 2*deltaX
+        b0 = point_init[1]
+        b1 = alfa_init*a1
+        b3 = deltaY - alfa_init*a1 - b2
+    else:
+        print('iv')
+        a1 = deltaX
+        a2 = 0
+        a0 = point_init[0]
+        a3 = deltaX - a1 - a2
+        b0 = point_init[1]
+        b1 = alfa_init*a1
+        b2 = 3*(deltaY-alfa_end*deltaX) + 2*(alfa_end-alfa_init)*a1 + alfa_end*a2
+        b3 = 3*alfa_end*deltaX - 2*deltaY - (2*alfa_end-alfa_init)*a1 -  alfa_end*a2
+
+    
+
+    result = []
+    x  = np.arange(0,1,0.01)
+    for i in range(len(x)):
+        fx = a0 + a1*x[i] + a2*x[i]^2 + a3*x[i]^3
+        fy = b0 + b1*x[i] + b2*x[i]^2 + b3*x[i]^3
+        theta_final = math.atan(fy/fx)
+        result.append((fx, fy, theta_final))
+    return result
 
 try:
     import sim
@@ -22,6 +89,7 @@ except:
 
 import time
 
+sim.path
 print ('Program started')
 sim.simxFinish(-1) # just in case, close all opened connections
 clientID=sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
@@ -36,6 +104,9 @@ if clientID!=-1:
         print ('Remote API function call returned with error code: ',res)
 
     time.sleep(2)
+
+    caminho = polinomio(point_init,point_end)
+
 
     # Now retrieve streaming data (i.e. in a non-blocking fashion):
     startTime=time.time()
